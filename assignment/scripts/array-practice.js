a1989241e3afffe7855edd8f1d905d7dbdb23e68
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-01-30 13:36:24 -0600

    myFavFood array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -9,10 +9,13 @@ console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
 
+let myFavFoods = ['Hot Cheetos', 'Mixed nuts', 'Pomegranate'];
+
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
 
+console.log('Some of my favorite foods are:', myFavFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
