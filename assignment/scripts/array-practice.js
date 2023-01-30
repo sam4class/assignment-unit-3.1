c495f79c98873536a840d5f1484849a78a6e293f
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-01-30 13:43:25 -0600

    number of Fav foods

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -26,6 +26,7 @@ console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
 
+console.log('Number of food items:', myFavFoods.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
