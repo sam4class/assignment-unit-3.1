81bd2dee8a96fac5e9ecade8533ece460a98d823
Samantha Marlowe <sam.c.marlowe@gmail.com>
2023-01-30 15:15:35 -0600

    used .join

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -106,6 +106,7 @@ console.log(reverseList);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+console.log(myFavFoods.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
